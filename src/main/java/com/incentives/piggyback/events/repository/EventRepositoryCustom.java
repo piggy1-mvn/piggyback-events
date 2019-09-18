0423b8f01e3e74340943de6348d1a13a7b9dfa68
Dipty Ojha <e0401948@u.nus.edu>
2019-09-18 21:32:54 +0800

    get event by partnerId , eventType and timestamp

---
+++ src/main/java/com/incentives/piggyback/events/repository/EventRepositoryCustom.java
@@ -0,0 +1,20 @@
+package com.incentives.piggyback.events.repository;
+
+
+import com.incentives.piggyback.events.dto.EventEntity;
+
+import java.util.Date;
+import java.util.List;
+
+public interface EventRepositoryCustom {
+
+    List<EventEntity> getEventsByEventPartnerType(String eventType, String PartnerId);
+
+    List<EventEntity> getEventsByEventType(String eventType);
+
+    List<EventEntity> getEventsByPartnerType(String PartnerId);
+
+    List<EventEntity> getEventsByEventAllFilters(String eventType, String PartnerId, Date fromDate, Date toDate);
+
+    List<EventEntity> getEventsByTimeStamp(Date fromDate, Date toDate);
+}
