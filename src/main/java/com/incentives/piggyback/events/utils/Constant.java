2e7d18627c8aa86871b009975f5b7e8d0a6a230c
Ashish Gupta <ashishddgr8@gmail.com>
2019-09-27 14:44:14 +0800

    events fix on prod

--- src/main/java/com/incentives/piggyback/events/utils/Constant.java
+++ src/main/java/com/incentives/piggyback/events/utils/Constant.java
@@ -9,10 +9,4 @@ public interface Constant {
 	String EVENT_SERVICE_PARTNER_SUBSCRIBER = "partner.subscriber.topic";
 	String EVENT_SERVICE_OFFERS_SUBSCRIBER = "offer.subscriber.topic";
 	String EVENT_SERVICE_NOTIFICATION_SUBSCRIBER = "notification.subscriber.topic";
-
-	String LOCATION_PUBLISHER_TOPIC = "location.publisher.topic";
-	String USER_PUBLISHER_TOPIC = "user.publisher.topic";
-	String PARTNER_PUBLISHER_TOPIC = "partner.publisher.topic";
-	String OFFERS_PUBLISHER_TOPIC = "offer.publisher.topic";
-	String NOTIFICATION_PUBLISHER_TOPIC = "notification.publisher.topic";
 }
