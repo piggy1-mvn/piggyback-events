5f7d6d7a029265f086594668f6538ee5cd94a6a0
Ashish Gupta <ashishddgr8@gmail.com>
2019-09-30 17:23:23 +0800

    return list

--- src/main/java/com/incentives/piggyback/events/controller/EventsController.java
+++ src/main/java/com/incentives/piggyback/events/controller/EventsController.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,11 +20,10 @@ public class EventsController {
 	private EventService eventService;
 
 	@GetMapping
-	public ResponseEntity<List<EventEntity>> getEvent(
+	public List<EventEntity> getEvent(
 			@RequestParam(name ="eventType", required = false) String eventType,
 			@RequestParam(name="partnerId", required = false) String partnerId,
 			@RequestParam(name="timeStamp", required = false)  Long timestamp) {
-		List<EventEntity> events = eventService.getEvents(eventType,partnerId, new Date(timestamp));
-		return ResponseEntity.ok(events);
+		return eventService.getEvents(eventType,partnerId, new Date(timestamp));
 	}
 }
