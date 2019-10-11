29338e88f373c69f1d9a68d22b03e9b0848b4dd9
Rajan Gupta <rajan0493@gmail.com>
2019-10-12 00:10:10 +0800

    Kafka Migration

--- src/main/java/com/incentives/piggyback/events/utils/Constant.java
+++ src/main/java/com/incentives/piggyback/events/utils/Constant.java
@@ -3,11 +3,13 @@ package com.incentives.piggyback.events.utils;
 public interface Constant {
 	Integer SUCCESS_STATUS = 200;
 	Integer FAILURE_STATUS = 101;
-	
-	String EVENT_SERVICE_LOCATION_SUBSCRIBER = "location.subscriber.topic";
-	String EVENT_SERVICE_USER_SUBSCRIBER = "user.subscriber.topic";
-	String EVENT_SERVICE_PARTNER_SUBSCRIBER = "partner.subscriber.topic";
-	String EVENT_SERVICE_OFFERS_SUBSCRIBER = "offer.subscriber.topic";
-	String EVENT_SERVICE_NOTIFICATION_SUBSCRIBER = "notification.subscriber.topic";
-	String EVENT_SERVICE_ORDER_SUBSCRIBER = "order.subscriber.topic";
+
+	String KAFKA_BOOTSTRAP_ADDRESS = "kafka.bootstrap.address";
+
+	String EVENT_SERVICE_LOCATION_SUBSCRIBER = "location.subscriber";
+	String EVENT_SERVICE_USER_SUBSCRIBER = "user.subscriber";
+	String EVENT_SERVICE_PARTNER_SUBSCRIBER = "partner.subscriber";
+	String EVENT_SERVICE_OFFERS_SUBSCRIBER = "offer.subscriber";
+	String EVENT_SERVICE_NOTIFICATION_SUBSCRIBER = "notification.subscriber";
+	String EVENT_SERVICE_ORDER_SUBSCRIBER = "order.subscriber";
 }
